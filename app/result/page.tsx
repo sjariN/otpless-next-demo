3cdf566a8ef6202387549fbddbd063c853f0da61
sjariN <sagar.jariwala@otpless.com>
2024-01-10 16:56:04 +0530

    login page added

---
+++ app/result/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+
+const Result = () => {
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    setToken(storedToken as any);
+  }, []);
+
+  return (
+    <main className='main'>token: {token}</main>
+  );
+};
+
+export default Result;
