ff932e89accbf45bc3535807983e0a4cbd1c0fcb
sjariN <sagar.jariwala@otpless.com>
2024-01-24 22:13:27 +0530

    updated readme

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,19 @@
 "use client";
-import Image from 'next/image'
-import styles from './page.module.css'
-import { initOTPless } from '@/utils/initOtpless'
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation';
+import styles from "./page.module.css";
+import { initOTPless } from "@/utils/initOtpless";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  useEffect(()=>initOTPless(handleOtplessData),[])
+  useEffect(() => initOTPless(handleOtplessData), []);
   const router = useRouter();
-const handleOtplessData = (otplessUser : any) =>{
-  localStorage.setItem('token',otplessUser.token);
-  router.push('/result');
-}
+  const handleOtplessData = (otplessUser: any) => {
+    localStorage.setItem("token", otplessUser.token);
+    router.push("/result");
+  };
   return (
     <main className={styles.main}>
       <div id="otpless-login-page"></div>
     </main>
-  )
+  );
 }
--- utils/initOtpless.ts
+++ utils/initOtpless.ts
@@ -1,11 +1,12 @@
 export const initOTPless = (callback: Function) => {
-
+    console.log("called init")
     const otplessInit = Reflect.get(window, 'otplessInit');
 
     const loadScript = () =>{
         const script = document.createElement("script")
         script.src = "https://otpless.com/auth.js";
         script.id = "otplessIdScript";
+        script.setAttribute("cid","YOUR_CID");
         document.body.appendChild(script)
     }
 
